52fbfad64d27437513f20ae956d92d6922e892cb
Zaydiscool777 <Zaydiscool777@gmail.com>
2023-12-14 20:25:03 -0500

    csv installments

--- main.py
+++ main.py
@@ -1,15 +1,37 @@
 import csv
 here = None
-def ask(*options, question="What do you do?"):
+doing = None
+f = open("data.csv", 'r+')
+read = [i[1] for i in csv.reader(f)]
+
+def ask(*options: tuple, question="What do you do?", other="") -> str:
     print("\t\b", question, '\n\t')
     for i in options:
         print(f"\t{i[0]}: {i[1]}")
-    return input('\t:o~~>')
-class Scene:
-    def __init__(self, location, nickname, next):
-        self.location = location
-        self.next = next
+    if (x := input('\tة<( ')) in options: return x
+    else: return other
+class Action:
+    def __init__(self, nickname: str, str: str, opt: tuple, print: bool) -> None:
         self.nickname = nickname
-    def move(self):
-        ask(*[i[0], i[1] for i in self.next], question=f"Where do you want to go from {self.nickname}?")
+        self.str = str
+        self.opt = opt
+        self.print = print
+        self.question = "What do you want to do now?"
+    def next(self):
+        if self.print:
+            print(self.str)
+        going(ask(*self.opt, self.question)) if not self.question == None else going(ask(*self.opt))
+class Scene(Action):
+    def __init__(self, nickname, desc, next):
+        super().__init__(nickname, desc, next, True)
+        self.question = f"Where do you want to go from {self.nickname}?"
+    def forth(self, to):
+        self.next.append(to)
+    def link(self, to):
+        self.forth(to)
+        to.forth(self)
+
+def do(where):
+    here = where
+
         
--- test.py
+++ test.py
@@ -2,3 +2,4 @@ def bla(*blah):
     for i in blah:
         print(i)
 bla(*[i for i in range(5)])
+del bla
